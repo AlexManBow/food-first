a5cd556b7af3fea5640e2d4374b3dbc81fdd7457
Alex <manbow@mail.ru>
2023-07-07 11:18:44 +0300

    10.004 Компонент Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,18 @@
+import { Link } from 'react-router-dom'
+
 function Header() {
 	return (
 		<nav className='green darken-1'>
 			<div className='nav-wrapper'>
-				<a
-					href='!#'
-					className='brand-logo'
-				>
+				<Link to='/' className='brand-logo'>
 					React Shop
-				</a>
-				<ul
-					id='nav-mobile'
-					className='right hide-on-med-and-down'
-				>
+				</Link>
+				<ul id='nav-mobile' className='right hide-on-med-and-down'>
 					<li>
-						<a href='!#'>Sass</a>
+						<Link to='/about'>About</Link>
+					</li>
+					<li>
+						<Link to='/contacts'>Contacts</Link>
 					</li>
 				</ul>
 			</div>
