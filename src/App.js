945779782d4d6b8c6484acef23cf10d655b27aa2
Alex <manbow@mail.ru>
2023-07-07 12:25:33 +0300

    10.005 «Хуки» useParams(), useHistory()

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Footer } from './components/Footer'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Contact } from './pages/Contact'
+import { Movie } from './pages/Movie'
 import { NotFound } from './pages/NotFound'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 					<Route exact path='/' component={Home} />
 					<Route path='/about' component={About} />
 					<Route path='/contacts' component={Contact} />
+					<Route path='/movies/:title' component={Movie} />
 					<Route component={NotFound} />
 				</Switch>
 			</main>
